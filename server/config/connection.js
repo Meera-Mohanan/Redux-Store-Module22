4d8224b3b75811c1d1d9b677ef4278b529d53f31
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-09-01 20:06:28 +0800

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/redux-store');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/redux-store-state"
+);
 
 module.exports = mongoose.connection;
